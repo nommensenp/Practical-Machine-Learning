db3dd3a6fa9af6a7c67896859b9b69008c901327
nommensenp <Paul.Nommensen@AVEBE.com>
2014-06-20 09:01:07 +0200

    init project

--- code/initProject.R
+++ code/initProject.R
@@ -1,5 +1,22 @@
 #
 #
 #
+library(caret)
 
-a<-1
+data <-  read.csv("data/pml-training.csv", na.strings = c("NA", "", "#DIV/0!"))
+
+# remove columns that are filled mainly with NA's
+countNA <- function(x) (sum(is.na(x)))
+noNA <- (sapply(data,countNA)==0)
+data <- data[,noNA]
+
+# split data in training-testing and validation sets
+set.seed(43563)
+intrain <- createDataPartition(y = data$classe, p=0.6, list=FALSE)
+training <- data[intrain,]
+testing <-  data[-intrain,]
+
+invalidate <- createDataPartition(y = testing$classe, p=0.5, list = FALSE)
+validate   <- testing[invalidate,]
+testing    <- testing[-invalidate,]
+
